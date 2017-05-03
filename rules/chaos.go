4d373124da371a4cbdf170b16578333d9a0f55a2
brianberzins <brian.berzins@target.com>
2017-05-03 15:03:43 -0500

    simplified chaos rule

--- rules/chaos.go
+++ rules/chaos.go
@@ -13,12 +13,10 @@ const ENV_CHAOS_CHANCE = "CHAOS_CHANCE"
 
 type chaos struct {
 	chance float64
-	random *rand.Rand
 }
 
-func createRand() *rand.Rand {
-	seed := time.Now().UnixNano() // no need for crypto-level randomness
-	return rand.New(rand.NewSource(seed))
+func init() {
+	rand.Seed(time.Now().UnixNano())
 }
 
 func (rule *chaos) load() (bool, error) {
@@ -30,11 +28,10 @@ func (rule *chaos) load() (bool, error) {
 	if err != nil {
 		return false, fmt.Errorf("invalid chaos chance %s", err)
 	}
-	rule.random = createRand()
 	rule.chance = chance
 	return true, nil
 }
 
 func (rule *chaos) ShouldReap(pod v1.Pod) (bool, string) {
-	return rule.random.Float64() < rule.chance, "was flagged for chaos"
+	return rand.Float64() < rule.chance, "was flagged for chaos"
 }
